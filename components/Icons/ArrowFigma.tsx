213c8435565d38636a704393cbc141072c29ecc6
Richard V. Narvaez <richard.bvn997@gmail.com>
2023-11-12 19:11:01 -0500

    Update: V3 of HomePage

---
+++ components/Icons/ArrowFigma.tsx
@@ -0,0 +1,56 @@
+export default function ArrowFigma(props) {
+  return (
+    <svg
+      width="37"
+      height="36"
+      viewBox="0 0 37 36"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <g filter="url(#filter0_d_107_3640)">
+        <path
+          d="M31.1659 6.18824L6.96703 0.92436C5.39894 0.58326 4.08791 2.13497 4.68603 3.6241L13.7363 26.1561C14.4843 28.0185 17.2029 27.7416 17.5602 25.7667L19.4305 15.4285C19.5573 14.7276 20.0472 14.1471 20.7168 13.9043L31.4225 10.0228C33.3082 9.33909 33.1259 6.61459 31.1659 6.18824Z"
+          fill="#F1F1F1"
+        />
+      </g>
+      <defs>
+        <filter
+          id="filter0_d_107_3640"
+          x="0.538574"
+          y="0.876709"
+          width="36.2026"
+          height="34.5345"
+          filterUnits="userSpaceOnUse"
+          color-interpolation-filters="sRGB"
+        >
+          <feFlood flood-opacity="0" result="BackgroundImageFix" />
+          <feColorMatrix
+            in="SourceAlpha"
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+            result="hardAlpha"
+          />
+          <feOffset dy="4" />
+          <feGaussianBlur stdDeviation="2" />
+          <feComposite in2="hardAlpha" operator="out" />
+          <feColorMatrix
+            type="matrix"
+            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+          />
+          <feBlend
+            mode="normal"
+            in2="BackgroundImageFix"
+            result="effect1_dropShadow_107_3640"
+          />
+          <feBlend
+            mode="normal"
+            in="SourceGraphic"
+            in2="effect1_dropShadow_107_3640"
+            result="shape"
+          />
+        </filter>
+      </defs>
+    </svg>
+  )
+}
