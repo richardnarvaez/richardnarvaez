6561bae9788139f54659893692849f6c0264fc02
Richard V. Narvaez <richard.bvn997@gmail.com>
2024-01-05 13:11:52 -0500

    add: metadata images to layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,6 +61,14 @@ export const metadata = {
     title: siteConfig.name,
     description: siteConfig.description,
     siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.title,
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
