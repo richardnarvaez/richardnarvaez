85b532b19ae276dbe8d4e2600245c80d7f6e0021
Richard V. Narvaez <richard.bvn997@gmail.com>
2024-01-05 16:20:05 -0500

    update: image source RESUME in home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -308,7 +308,7 @@ export default async function IndexPage() {
             alt=""
             className="absolute inset-0 h-full w-full object-cover transition-transform ease-out group-hover:scale-110 group-hover:blur-sm"
             src={
-              "https://images.unsplash.com/photo-1536104968055-4d61aa56f46a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80"
+              "/images/home/resume.webp"
             }
             width={700}
             height={900}
