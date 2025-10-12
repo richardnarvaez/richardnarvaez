301c7328adc8f37c93df2b8d740c392923172da0
Richard Vinueza <richardvnarvaez@gmail.com>
2025-10-12 14:47:54 +0200

    chore: update contentlayer configuration by commenting out unused rehype plugins, update pnpm-lock.yaml with shiki and rehype-pretty-code version changes, and clean up tsconfig.json by removing skipLibCheck

--- contentlayer.config.js
+++ contentlayer.config.js
@@ -1,7 +1,7 @@
 import { defineDocumentType, makeSource } from "contentlayer/source-files"
-import rehypeAutolinkHeadings from "rehype-autolink-headings"
-import rehypePrettyCode from "rehype-pretty-code"
-import rehypeSlug from "rehype-slug"
+// import rehypeAutolinkHeadings from "rehype-autolink-headings"
+// import rehypePrettyCode from "rehype-pretty-code"
+// import rehypeSlug from "rehype-slug"
 import remarkGfm from "remark-gfm"
 
 /** @type {import('contentlayer/source-files').ComputedFields} */
@@ -104,36 +104,36 @@ export default makeSource({
   documentTypes: [Page, Post, Author],
   mdx: {
     remarkPlugins: [remarkGfm],
-    rehypePlugins: [
-      rehypeSlug,
-      [
-        rehypePrettyCode,
-        {
-          theme: "github-dark",
-          onVisitLine(node) {
-            // Prevent lines from collapsing in `display: grid` mode, and allow empty
-            // lines to be copy/pasted
-            if (node.children.length === 0) {
-              node.children = [{ type: "text", value: " " }]
-            }
-          },
-          onVisitHighlightedLine(node) {
-            node.properties.className.push("line--highlighted")
-          },
-          onVisitHighlightedWord(node) {
-            node.properties.className = ["word--highlighted"]
-          },
-        },
-      ],
-      [
-        rehypeAutolinkHeadings,
-        {
-          properties: {
-            className: ["subheading-anchor"],
-            ariaLabel: "Link to section",
-          },
-        },
-      ],
-    ],
+    // rehypePlugins: [
+    //   rehypeSlug,
+    //   // [
+    //   //   // rehypePrettyCode,
+    //   //   {
+    //   //     theme: "github-dark",
+    //   //     onVisitLine(node) {
+    //   //       // Prevent lines from collapsing in `display: grid` mode, and allow empty
+    //   //       // lines to be copy/pasted
+    //   //       if (node.children.length === 0) {
+    //   //         node.children = [{ type: "text", value: " " }]
+    //   //       }
+    //   //     },
+    //   //     onVisitHighlightedLine(node) {
+    //   //       node.properties.className.push("line--highlighted")
+    //   //     },
+    //   //     onVisitHighlightedWord(node) {
+    //   //       node.properties.className = ["word--highlighted"]
+    //   //     },
+    //   //   },
+    //   // ],
+    //   [
+    //     rehypeAutolinkHeadings,
+    //     {
+    //       properties: {
+    //         className: ["subheading-anchor"],
+    //         ariaLabel: "Link to section",
+    //       },
+    //     },
+    //   ],
+    // ],
   },
 })
